acacb338af20ad81d8f76d74e7a2fdbe18fe9ea5
Matthew Strickland <mpstrick@gmail.com>
2011-04-19 18:47:19 -0400

    initial commit.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  
+  def index
+  end
+  
+  def gospel
+  end
+  
+  def worship
+  end
+  
+  def missions
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nowwhat::Application.config.secret_token = '085752deeb695b6302d1fb9acb675aeb78425f0b12c8ec8a7acdea329e06b8b1525fe576986f99b059cd636f8ed2e1740ecd4f9c70a72506c5c798d58d852ab7'
